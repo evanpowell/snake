5aabe3f2471d39bce8970612ea06367a564eba00
evanpowell <evanpowellmusic@gmail.com>
2019-03-30 17:42:30 -0500

    change folder name

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Title from './components/Home/Title';
+import Title from './components/Title/Title';
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <Route path="/" exact component={Title}></Route>
-      </Router>
+      <div className="container">
+        <Router>
+          <Route path="/" exact component={Title}></Route>
+        </Router>
+      </div>
     )
   }
 }
