8c95c4681e2d5949ac403f135fe3e6e74345645f
evanpowell <evanpowellmusic@gmail.com>
2019-03-30 17:46:22 -0500

    implement initial state

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,20 +1,35 @@
-const TOGGLE_WALL = '';
+const TOGGLE_WALL = 'TOGGLE_WALL';
+const CHANGE_SPEED = 'CHANGE_SPEED';
+const CHANGE_SPEED = 'CHANGE_SPEED';
+const CHANGE_COLORS = 'CHANGE_COLORS';
 
 const initialState = {
   highScores: {
     
   },
   isWall: true,
-  speed: 2
-}
+  speed: 2,
+  colors: {
+    snake: '#0bdd1d',
+    food: '#e00606',
+    screen: '#111',
+    background: '#111'
+  }
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
 
   case TOGGLE_WALL:
-    return { ...state, ...payload }
+    return { ...state, isWall: !state.isWall };
+
+  case CHANGE_SPEED:
+    return { ...state, ...payload };
+
+  case CHANGE_COLORS:
+    return { ...state, colors: { ...this.state.colors, ...payload }};
 
   default:
-    return state
+    return state;
   }
 }
