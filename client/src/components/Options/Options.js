7ffd1f287e286a5d041f90e135f918902bac682d
evanpowell <evanpowellmusic@gmail.com>
2019-04-04 18:20:29 -0500

    update styles

--- client/src/components/Options/Options.js
+++ client/src/components/Options/Options.js
@@ -19,86 +19,92 @@ export class Options extends Component {
 
   render() {
     return (
-      <div className="text-center">
+      <div className="text-center h-100">
 
-        <h3 className="heading mb-5" style={{ color: this.props.textColor }}>Settings</h3>
+        <div className="menu menu__heading menu__heading--settings">
+          <h3 className="heading" style={{ color: this.props.textColor }}>Settings</h3>
+        </div>
 
-        <div className="option mb-5">
+        <div className="menu menu__options menu__options--settings">
 
-          <div 
+          <div className="option__label">
+
+            <div 
+              className="option__active option__active--left"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'gameplay' ? 'inline-block' : 'none' }}
+            ></div>
+
+            <h2>
+              <Link
+                to="/settings/gameplay"
+                className="router-link option__text"
+                id="gameplay"
+                style={{ color: this.props.textColor }}
+                onFocus={this.handleFocus}
+              >
+                Gameplay
+              </Link>
+            </h2>
+
+            <div
+              className="option__active option__active--right"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'gameplay' ? 'inline-block' : 'none' }}
+            ></div>
+
+          </div>
+
+          <div className="option__label">
+
+            <div
             className="option__active option__active--left"
-            style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'gameplay' ? 'inline-block' : 'none' }}
-          ></div>
-
-          <h2>
-            <Link
-              to="/settings/gameplay"
-              className="title--menu option__text"
-              id="gameplay"
-              style={{ color: this.props.textColor }}
-              onFocus={this.handleFocus}
-            >
-              Gameplay
-            </Link>
-          </h2>
-
-          <div
-            className="option__active option__active--right"
-            style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'gameplay' ? 'inline-block' : 'none' }}
-          ></div>
-
-        </div>
-
-        <div className="option mb-5">
-
-          <div
-          className="option__active option__active--left"
-          style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'display' ? 'inline-block' : 'none' }}
-          ></div>
-          
-          <h2>
-            <Link
-              to="/settings/display"
-              className="title--menu option__text"
-              id="display"
-              style={{ color: this.props.textColor }}
-              onFocus={this.handleFocus}
-            >
-              Display
-            </Link>
-          </h2>
-
-          <div
-            className="option__active option__active--right"
             style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'display' ? 'inline-block' : 'none' }}
-          ></div>
+            ></div>
+            
+            <h2>
+              <Link
+                to="/settings/display"
+                className="router-link option__text"
+                id="display"
+                style={{ color: this.props.textColor }}
+                onFocus={this.handleFocus}
+              >
+                Display
+              </Link>
+            </h2>
 
-        </div>
+            <div
+              className="option__active option__active--right"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'display' ? 'inline-block' : 'none' }}
+            ></div>
 
-        <div className="option">
+          </div>
 
-          <div
-          className="option__active option__active--left"
-          style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'back' ? 'inline-block' : 'none' }}
-          ></div>
+          <div className="option__label">
 
-          <h2>
-            <Link
-              to="/"
-              className="title--menu option__text"
-              id="back"
-              style={{ color: this.props.textColor }}
-              onFocus={this.handleFocus}
-            >
-              Back
-            </Link>
-          </h2>
-          
-
-          <div
-            className="option__active option__active--right"
+            <div
+            className="option__active option__active--left"
             style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'back' ? 'inline-block' : 'none' }}
-          ></div>
+            ></div>
+
+            <h2>
+              <Link
+                to="/"
+                className="router-link option__text"
+                id="back"
+                style={{ color: this.props.textColor }}
+                onFocus={this.handleFocus}
+              >
+                Back
+              </Link>
+            </h2>
+            
+
+            <div
+              className="option__active option__active--right"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'back' ? 'inline-block' : 'none' }}
+            ></div>
+
+          </div>
 
         </div>
 
--- client/src/components/Title/Title.js
+++ client/src/components/Title/Title.js
@@ -19,61 +19,68 @@ export class Title extends Component {
 
   render() {
     return (
-      <div className="text-center">
+      <div className="text-center h-100">
 
-        <h1 className="title" style={{ color: this.props.titleColor }}>Snake</h1>
+        <div className="menu menu__heading menu__heading--title">
+          <h1 style={{ color: this.props.titleColor }}>Snake</h1>
+        </div>
 
-        <div className="option mb-5">
+        <div className="menu menu__options menu__options--title">
 
-          <div 
-            className="option__active option__active--left"
-            style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'start' ? 'inline-block' : 'none' }}
-          ></div>
+          <div className="option__label">
 
-          <h2>
-            <Link
-              to="/game"
-              className="title--menu option__text"
-              id="start"
-              style={{ color: this.props.menuColor }}
-              onFocus={this.handleFocus}
-            >
-              Start
-            </Link>
-          </h2>
+            <div 
+              className="option__active option__active--left"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'start' ? 'inline-block' : 'none' }}
+            ></div>
 
-          <div
-            className="option__active option__active--right"
-            style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'start' ? 'inline-block' : 'none' }}
-          ></div>
+            <h2>
+              <Link
+                to="/game"
+                className="router-link option__text"
+                id="start"
+                style={{ color: this.props.menuColor }}
+                onFocus={this.handleFocus}
+              >
+                Start
+              </Link>
+            </h2>
+
+            <div
+              className="option__active option__active--right"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'start' ? 'inline-block' : 'none' }}
+            ></div>
+
+          </div>
+
+          <div className="option__label">
+
+            <div
+              className="option__active option__active--left"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'settings' ? 'inline-block' : 'none' }}
+            ></div>
+            
+            <h2>
+              <Link
+                to="/settings"
+                className="router-link option__text"
+                id="settings"
+                style={{ color: this.props.menuColor }}
+                onFocus={this.handleFocus}
+              >
+                Settings
+              </Link>
+            </h2>
+
+            <div
+              className="option__active option__active--right"
+              style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'settings' ? 'inline-block' : 'none' }}
+            ></div>
+
+          </div>
 
         </div>
 
-        <div className="option">
-
-          <div
-          className="option__active option__active--left"
-          style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'settings' ? 'inline-block' : 'none' }}
-          ></div>
-          
-          <h2>
-            <Link
-              to="/settings"
-              className="title--menu option__text"
-              id="settings"
-              style={{ color: this.props.menuColor }}
-              onFocus={this.handleFocus}
-            >
-              Settings
-            </Link>
-          </h2>
-
-          <div
-            className="option__active option__active--right"
-            style={{ backgroundColor: this.props.focusColor, display: this.state.focusedOption === 'settings' ? 'inline-block' : 'none' }}
-          ></div>
-
-        </div>
 
       </div>
     )
