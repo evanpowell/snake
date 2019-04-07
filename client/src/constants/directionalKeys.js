ef8eb2f8a11a8ce7dc8a47790e15265c1e3986fe
evanpowell <evanpowellmusic@gmail.com>
2019-04-06 21:13:21 -0500

    add direction keys constants object

---
+++ client/src/constants/directionalKeys.js
@@ -0,0 +1,19 @@
+export const upKeys = {
+  'ArrowUp': true,
+  'w': true,
+}
+
+export const downKeys = {
+  'ArrowDown': true,
+  's': true,
+}
+
+export const leftKeys = {
+  'ArrowLeft': true,
+  'a': true,
+}
+
+export const rightKeys = {
+  'ArrowRight': true,
+  'd': true,
+}
