43ceb914fa4640247ec4db775e67e862e2687133
evanpowell <evanpowellmusic@gmail.com>
2019-04-06 21:15:18 -0500

    add gameplay route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import Title from './components/Title/Title';
 import Options from './components/Options/Options';
+import Gameplay from './components/Options/Gameplay/Gameplay';
 
 export class App extends Component {
   componentDidMount() {
@@ -31,6 +32,7 @@ export class App extends Component {
                 <Switch>
                   <Route path="/" exact component={Title}></Route>
                   <Route path="/settings" exact component={Options}></Route>
+                  <Route path="/settings/gameplay" exact component={Gameplay}></Route>
                 </Switch>
               </Router>
             </div>
